8cc77f06ac346139ba3d1b6b2167cfe3409da5f3
Joonas <joonas.aapro@mainiotech.fi>
2025-06-25 16:38:16 +0300

    Fix login dropdown when wrong credentials

--- app/packs/src/decidim/simple_ui/login.js
+++ app/packs/src/decidim/simple_ui/login.js
@@ -2,6 +2,12 @@ $(() => {
   const revealButton = document.querySelector(".register__separator");
   const loginForm = document.getElementById("session_new_user");
   const svgIcon = revealButton.querySelector("svg");
+  const alert = document.querySelector(".flash.alert");
+
+  if (alert) {
+    loginForm.classList.toggle("hidden");
+    svgIcon.classList.toggle("rotate-180");
+  }
 
   revealButton.addEventListener("click", () => {
     loginForm.classList.toggle("hidden");
