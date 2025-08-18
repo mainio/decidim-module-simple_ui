bf9bc3b0ee8f6a92cb1965a53eb35a6fb1fab8cb
Antti Hukkanen <antti.hukkanen@mainiotech.fi>
2025-08-18 09:52:08 +0300

    Fix an error in the login.js

--- app/packs/src/decidim/simple_ui/login.js
+++ app/packs/src/decidim/simple_ui/login.js
@@ -1,5 +1,9 @@
-$(() => {
+document.addEventListener("DOMContentLoaded", () => {
   const revealButton = document.querySelector(".register__separator");
+  if (!revealButton) {
+    return;
+  }
+
   const loginForm = document.getElementById("session_new_user");
   const svgIcon = revealButton.querySelector("svg");
   const alert = document.querySelector("header>.container>.flash.alert");
@@ -13,4 +17,4 @@ $(() => {
     loginForm.classList.toggle("hidden");
     svgIcon.classList.toggle("rotate-180");
   })
-})
+});
