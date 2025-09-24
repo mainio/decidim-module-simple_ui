2755a41482c28ff41881c81b977087ac90e7b4cc
Joonas <joonas.aapro@mainiotech.fi>
2025-09-24 07:49:23 +0300

    Move CSS and views from city instance

--- config/assets.rb
+++ config/assets.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "decidim/core"
+
 base_path = File.expand_path("..", __dir__)
 
 Decidim::Webpacker.register_path("#{base_path}/app/packs", prepend: true)
@@ -9,3 +11,6 @@ Decidim::Webpacker.register_entrypoints(
   decidim_simple_ui_content_blocks_admin: "#{base_path}/app/packs/entrypoints/decidim_simple_ui_content_blocks_admin.js",
 )
 Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/simple_ui/simple_ui")
+
+Decidim::Webpacker.register_stylesheet_import("stylesheets/decidim/budgets/budgets") unless Decidim.module_installed? :budgets_booth
+
