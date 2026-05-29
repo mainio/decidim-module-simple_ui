8a8996612ed0c75972aabe0ee0b53231a5f300ce
Joonas <joonas.aapro@mainiotech.fi>
2026-05-29 13:40:45 +0300

    Update component intro to show links correctly

--- app/helpers/decidim/simple_ui/application_helper.rb
+++ app/helpers/decidim/simple_ui/application_helper.rb
@@ -5,6 +5,15 @@ module Decidim
     # Custom helpers, scoped to the simple_ui engine.
     #
     module ApplicationHelper
+      def style_rich_text(html)
+        return if strip_tags(html).blank?
+
+        doc = Nokogiri::HTML::DocumentFragment.parse(html)
+        doc.css("p").each { |p| p["class"] = "inline-block text-xl text-left lg:text-center lg:w-4/6" }
+        doc.css("a").each { |a| a["class"] = "text-[--primary] font-semibold underline" }
+
+        doc.to_html.html_safe
+      end
     end
   end
 end
