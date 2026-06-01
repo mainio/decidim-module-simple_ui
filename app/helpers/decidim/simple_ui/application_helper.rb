ad4de581e0bab9a2aa9f05242743f9546285a500
Joonas <joonas.aapro@mainiotech.fi>
2026-06-01 10:03:32 +0300

    Fix component intro CSS

--- app/helpers/decidim/simple_ui/application_helper.rb
+++ app/helpers/decidim/simple_ui/application_helper.rb
@@ -9,7 +9,7 @@ module Decidim
         return if strip_tags(html).blank?
 
         doc = Nokogiri::HTML::DocumentFragment.parse(html)
-        doc.css("p").each { |p| p["class"] = "inline-block text-xl text-left lg:text-center lg:w-4/6" }
+        doc.css("p").each { |p| p["class"] = "text-xl text-left md:text-center lg:w-4/6" }
         doc.css("a").each { |a| a["class"] = "text-[--primary] font-semibold underline" }
 
         doc.to_html.html_safe
