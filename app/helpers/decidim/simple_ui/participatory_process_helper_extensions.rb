a93ba50adad29370a980461dddb70facbfb19649
Joonas <joonas.aapro@mainiotech.fi>
2026-06-04 08:00:21 +0300

    Update filter_sections attribute from label_scope to label

--- app/helpers/decidim/simple_ui/participatory_process_helper_extensions.rb
+++ app/helpers/decidim/simple_ui/participatory_process_helper_extensions.rb
@@ -8,7 +8,7 @@ module Decidim
       included do
         def filter_sections
           [
-            { method: :with_date, collection: filter_dates_values, label_scope: "decidim.participatory_processes.participatory_processes.filters", id: "process-date" }
+            { method: :with_date, collection: filter_dates_values, label: "decidim.participatory_processes.participatory_processes.filters", id: "process-date" }
           ].reject { |item| item[:collection].blank? }
         end
       end
